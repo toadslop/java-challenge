fb2422e1e826d34f6eeba60b40c31026b41b1544
Brian Heise <92563897+bnheise@users.noreply.github.com>
2023-03-28 07:46:22 +0900

    added comments to employee dto

--- src/main/java/jp/co/axa/apidemo/dto/EmployeeDto.java
+++ src/main/java/jp/co/axa/apidemo/dto/EmployeeDto.java
@@ -1,10 +1,14 @@
 package jp.co.axa.apidemo.dto;
 
+import jp.co.axa.apidemo.entities.Employee;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * DTO for the {@link Employee} class. Only contains writable fields.
+ */
 @Setter
 @Getter
 @NoArgsConstructor
